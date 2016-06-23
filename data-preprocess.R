abc240bb2defb62e204d3f26cda3d0c5384acc9a
Andrzej Wójtowicz <andre@amu.edu.pl>
2016-06-23 12:36:39 +0200

    Added extra preprocessing for column names and factor levels

--- data-preprocess.R
+++ data-preprocess.R
@@ -28,7 +28,27 @@ for (dir.name in dir(PATH_DATASETS))
     r.src.file = paste0(PATH_DATASETS, dir.name, "/", FILE_PREPROCESSING_SCRIPT)
     
     source(r.src.file)
-    dataset = preprocessDataset()
+    dataset = preprocessDataset() # custom per-dataset preprocessing
+    
+    # change column names
+    colnames(dataset) = tolower(
+                            make.names(
+                                gsub("^\\.|\\.$", "", colnames(dataset)),
+                            unique=TRUE, allow_=FALSE))
+    
+    # change factor levels
+    for (name in colnames(dataset))
+    {
+        if (any(class(dataset[[name]]) == "factor"))
+        {
+            levels(dataset[[name]]) = tolower(
+                                        make.names(
+                                            gsub("^\\.|\\.$", "", 
+                                                 levels(dataset[[name]])),
+                                            unique=TRUE, allow_=FALSE))
+            
+        }
+    }
     
     printDatasetStatistics(dataset)
     
